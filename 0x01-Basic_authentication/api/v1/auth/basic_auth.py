bf133f703f1d9c82502675d38242a82d2dac1a8c
Parsitau-michael <parsitaumichael@gmail.com>
2024-11-17 17:30:18 +0000

    Corrected pycodestyle error

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,13 +9,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ This is a class BasicAuth that inherits from Auth
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
         """ returns the Base64 part of the Authorization header
         for a Basic Authentication
         """
         if authorization_header is None:
             return None
-        
+
         if isinstance(authorization_header, str):
             if authorization_header.startswith("Basic "):
                 return authorization_header[6:]
