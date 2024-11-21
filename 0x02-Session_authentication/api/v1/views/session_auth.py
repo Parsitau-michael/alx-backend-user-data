ef5cc612696064e940e9cea39816da5cfdc25e71
Parsitau-michael <parsitaumichael@gmail.com>
2024-11-21 21:32:48 +0000

    Added a new Flask view that handles all routes for the Session authentication.

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" This module is a Flask view that handles all routes
+for the Session authentication
+"""
+
+
+from flask import request, jsonify
+from api.v1.views import app_views
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def session_auth_login():
+    """ Route definition
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({"email": email})
+
+    if not users or len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = users[0]
+
+    if not user.is_valid_password(password):
+        return ({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+
+    response = jsonify(user.to_json())
+
+    session_name = os.getenv("SESSION_NAME", "_my_session_id")
+    response.set_cookie(session_name, session_id)
+
+    return response
