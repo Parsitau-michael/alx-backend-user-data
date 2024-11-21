66fbe9ea2bc148c7913537819e20ba55ff17745d
Parsitau-michael <parsitaumichael@gmail.com>
2024-11-21 13:52:57 +0000

    Added a path /api/v1/auth_session/login/ in the list of excluded paths of the method require_auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,9 +57,10 @@ def request_filter():
     if auth is None:
         return
     request_list = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
+                    '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, request_list):
-        if auth.authorization_header(request) is None:
+        if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
