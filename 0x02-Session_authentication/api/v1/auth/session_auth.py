008ea029635bd7ee93c0825d035ffa83e5ca9c4e
Parsitau-michael <parsitaumichael@gmail.com>
2024-11-21 09:12:28 +0000

    Created an empty class SessionAuth that inherits from Auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module represents SessionAuth class definition
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class definition
+    """
+    pass
