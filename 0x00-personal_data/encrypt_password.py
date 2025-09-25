23beb06f02639f3ac18c5404972260ff0c22da3f
Parsitau-michael <parsitaumichael@gmail.com>
2025-09-25 19:07:06 +0000

    Implemented a function to Hash a password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A module that implements a hash_password function"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    A function to hash a password
+    Args:
+        password - a string representing user password
+    Returns - a salted, hashed password which is a byte string
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
