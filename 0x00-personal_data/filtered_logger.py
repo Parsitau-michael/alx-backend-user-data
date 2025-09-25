cd86340c920a5ab1cc37555630102d8919d4ed4f
Parsitau-michael <parsitaumichael@gmail.com>
2025-09-25 05:30:25 +0000

    Type anotated my filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,21 @@
 """ This module contains a function to obfuscate log messages"""
 
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
-    """Function to obfuscate PII fields in log files"""
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Function to obfuscate PII fields in log files
+
+    Args:
+        fields: List of field names to obfuscate
+        redaction: String to replace field values with
+        message: Log message to process
+        separator: Character separating fields in the message
+
+    Returns:
+        Obfuscated log message with PII fields redacted
+    """
     pattern = rf'({"|".join(fields)})=.*?({separator})'
     obfuscated = re.sub(pattern, r"\1="+redaction+r"\2", message)
     return obfuscated
